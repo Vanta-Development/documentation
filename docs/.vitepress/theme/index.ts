5d1095e5a029803805738e28432e6a8faac826d4
Vanta Development <vantadevelopment@proton.me>
2026-01-15 00:37:27 +0000

    Update index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,4 @@
-import DefaultTheme from "vitepress/theme";
-import "./custom.css";
+import DefaultTheme from 'vitepress/theme';
+import './custom.css'; // <-- your CSS
 
 export default DefaultTheme;
